224be156b8db887a326f6fd7750f310985263ef6
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-12-27 13:23:49 -0600

    firebase and context files added

---
+++ contexts/MaterialContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useState, useEffect } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import db from '../firebase/config';
+
+export const MaterialContext = createContext();
+
+export default function MaterialProvider({ children }) {
+	const [points, setPoints] = useState([]);
+	const [materials, setMaterials] = useState([]);
+	const [pallet, setPallet] = useState([]);
+	const [layers, setLayers] = useState({});
+
+	const selectPallet = (id) => {
+		const mats = materials.filter((mat) => mat.id === id);
+		setPallet(mats);
+	};
+
+	const addLayer = (layer) => {
+		const id = pallet[0].id;
+
+		setLayers((prev) => ({ ...prev, [id]: layer }));
+	};
+
+	useEffect(() => {
+		const ptsRef = collection(db, 'points');
+		const matsRef = collection(db, 'materials');
+
+		onSnapshot(ptsRef, (snap) => {
+			setPoints(snap.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		});
+
+		onSnapshot(matsRef, (snap) => {
+			setMaterials(snap.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+		});
+	}, []);
+
+	return (
+		<MaterialContext.Provider
+			value={{ points, materials, pallet, layers, selectPallet, addLayer }}
+		>
+			{children}
+		</MaterialContext.Provider>
+	);
+}
