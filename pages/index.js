afe82d834fc79b6ad88cd7a71a1b26809721501e
FlorentinoG9 <f.valle.garcia@gmail.com>
2021-12-27 14:03:48 -0600

    add headlessui for Dialog

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-export default function Home () {
-  return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      ESTUDIO CACTUS VISUALIZER TEST
-    </div>
-  )
+import Image from 'next/image';
+import MaterialProvider from '../contexts/MaterialContext';
+
+Image;
+export default function Home() {
+	return (
+		<div className='relative flex flex-col items-center justify-center min-h-screen backdrop-blur-sm'>
+			<div className='absolute'>
+				<Image
+					src='https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/projects%2FdorptVQTHsbkYC60NSlt%2Fscenes%2F1567170849457-base?alt=media&token=cf8bcee2-bf89-4fd9-8bfd-9d4462348844'
+					width={1250}
+					height={885}
+					priority
+				/>
+        <MaterialProvider>
+          
+        </MaterialProvider>
+			</div>
+		</div>
+	);
 }
